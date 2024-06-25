47cf426a730672e93b7f3ea048327456930b6714
Saba881 <gtssaba@gmail.com>
2024-06-25 22:29:30 +0400

    commit

---
+++ level 10/lvl10.py
@@ -0,0 +1,43 @@
+#for i in range (101):
+    #sum= i + 21
+    #sub= i - 40
+    #mul= i * 2
+    #div= i / 2
+
+#print(i)
+#print(sum)
+#print(sub)
+#print(mul)
+#print(div)
+
+
+
+
+
+#num = int(input("enter the number: "))
+
+#sum_of_numbers = 0
+
+#for i in range (1, num + 1):
+    #sum_of_numbers += i
+    #print(sum_of_numbers)
+
+
+
+
+
+
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+ 
+sum_result = 0
+mul_result= 1
+
+for num in (num1, num2) :
+
+    sum_result += num
+    mul_result *= num 
+
+print(sum_result)
+print(mul_result)
+
---
+++ level 8/level8.py
@@ -0,0 +1,80 @@
+#num1=float(input("enter the first number : "))
+#num2=float(input("enter the second number : "))
+#num3=float(input("enter the third number : "))
+#num4=float(input("enter the fourth number : "))
+#num5=float(input("enter the fifth number : "))
+
+#print(num1+num2+num3+num4+num5)
+#print(num1-num2-num3-num4-num5)
+#print(num1*num2*num3*num4*num5)
+#print(num1/num2/num3/num4/num5)
+#print(num1%num2%num3%num4%num5)
+#print(num1//num2//num3//num4//num5)
+
+#USER AGE 
+
+#user_age=int(input("enter user age : "))
+#if user_age> 18 and user_age < 20 :
+     #print("user age is more than 18 and less than 20")
+#else:
+    # print("user age does not meet the conditions")
+
+# >
+5>3
+10>8
+-7>-9
+194>182
+24.4>23.9
+# < 
+8<5
+-3<-5
+91<13
+41.3<41.2
+#<=
+3<=3
+5<=10
+-67<=-8
+6.1<=6.2
+4149<=4149
+#>=
+7>=3
+8>=8
+-7>=-9
+94.4>=94.4
+23>=11
+#!=
+7 != 4
+True != False 
+'apple' != 'pen'
+10 != 10.1
+-5 != 5
+#== 
+6==6
+True == True
+'dog'=='dog'
+1039==1039
+14==14
+
+
+#integer_input=input ("enter an integer : ")
+#float_input= input ("input float : ")
+
+#integer_type = type(int(integer_input))
+#float_type = type(float(float_input))
+
+#print(integer_type)
+#print(float_type)
+
+
+num1="saba"
+num2=7
+num3=9.1
+num4=4.2
+num5=0
+print=type(str(num1))
+
+
+
+
+
+
