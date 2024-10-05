dc6aed753e2b1af2909909c94c077f50a56dfa37
Saba881 <157902179+Saba881@users.noreply.github.com>
2024-10-05 22:59:19 +0400

    commit

---
+++ level 39/main.java
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 5;
+
+let sumResult = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+let name = "saba";
+let surname = "oqruashvili";
+let age = 17;
+
+let comparisonResult = 10 > 5;
+console.log(comparisonResult);
